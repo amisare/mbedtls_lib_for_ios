b2570c18024d35632a805828543c693ecd7fdba4
GuHaijun <243297288@qq.com>
2018-12-28 14:36:00 +0800

    #fix 打包脚本 mbedtls 版本指定 bug #modify 增加 target 库头文件 include 文件夹

--- build_lib.sh
+++ build_lib.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-VERSION_TAG="2.16.0"
+VERSION_TAG="2.7.9"
 
 create_directory() {
     rm -rf build/*
     rm -rf target/*
     mkdir -p build
     mkdir -p target/mbedtls/library/
+    mkdir -p target/mbedtls/include/
     mkdir -p target/mbedtls_device/library/
+    mkdir -p target/mbedtls_device/include/
     mkdir -p target/mbedtls_simulator/library/
+    mkdir -p target/mbedtls_simulator/include/
 }
 
 fetch_mbedtls() {
     git clone https://github.com/ARMmbed/mbedtls.git mbedtls
-    git -C mbedtls checkout ${VERSION_TAG}
+    git -C mbedtls checkout mbedtls-${VERSION_TAG}
+    git status
 }
 
 run_cmake() {
@@ -33,19 +37,19 @@ main_func() {
     run_cmake 'DEVICE'
     make
     cp -Rf ${DIR}/build/library/libmbe* ${DIR}/target/mbedtls_device/library/
-    cp -Rf ${DIR}/build/include/mbedtls/* ${DIR}/target/mbedtls_device/
+    cp -Rf ${DIR}/build/include/mbedtls/* ${DIR}/target/mbedtls_device/include/
     make clean
 
     run_cmake 'SIMULATOR'
     make
     cp -Rf ${DIR}/build/library/libmbe* ${DIR}/target/mbedtls_simulator/library/
-    cp -Rf ${DIR}/build/include/mbedtls/* ${DIR}/target/mbedtls_simulator/
+    cp -Rf ${DIR}/build/include/mbedtls/* ${DIR}/target/mbedtls_simulator/include/
     make clean
 
     lipo ${DIR}/target/mbedtls_device/library/libmbedcrypto.a ${DIR}/target/mbedtls_simulator/library/libmbedcrypto.a  -create -output ${DIR}/target/mbedtls/library/libmbedcrypto.a
     lipo ${DIR}/target/mbedtls_device/library/libmbedx509.a ${DIR}/target/mbedtls_simulator/library/libmbedx509.a  -create -output ${DIR}/target/mbedtls/library/libmbedx509.a
     lipo ${DIR}/target/mbedtls_device/library/libmbedtls.a ${DIR}/target/mbedtls_simulator/library/libmbedtls.a  -create -output ${DIR}/target/mbedtls/library/libmbedtls.a
-    cp -rn ${DIR}/target/mbedtls_device/*.h ${DIR}/target/mbedtls/
+    cp -rn ${DIR}/target/mbedtls_device/include/*.h ${DIR}/target/mbedtls/include/
 }
 
 main_func
